2221941618382dbd0bb252e72fc08ecfe9e56f52
Lyxke <lyxke@outlook.com>
2020-08-04 19:55:19 -0400

    Even more and more attempts to resolve the hunger damage issue #2.

--- src/main/java/io/github/lyxke/hangoutcore/events/PlayerHungerDamage.java
+++ src/main/java/io/github/lyxke/hangoutcore/events/PlayerHungerDamage.java
@@ -23,14 +23,13 @@ public class PlayerHungerDamage implements Listener {
          until they eat or reach .5 total health
          */
         if (player.getFoodLevel() <= 0) {
-            while(e.getEntity().getHealth() > .5){
+            while(e.getEntity().getHealth() > 1){
                 try {
                     TimeUnit.SECONDS.sleep(4);
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
                 }
-                e.getEntity().setHealth(e.getEntity().getHealth() - .5);
-
+                player.setHealth(player.getHealth() - 1);
             }
         }
     }
