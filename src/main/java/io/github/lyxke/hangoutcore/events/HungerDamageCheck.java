caa4eeb9837f6f9c80b8e1909ed52eb25d85d9a4
Lyxke <lyxke@outlook.com>
2020-08-05 14:03:33 -0400

    For Issue #2, the previous solution crashes the server because of the thread crashing. Trying this solution now.

--- src/main/java/io/github/lyxke/hangoutcore/events/HungerDamageCheck.java
+++ src/main/java/io/github/lyxke/hangoutcore/events/HungerDamageCheck.java
@@ -4,38 +4,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.concurrent.TimeUnit;
+import io.github.lyxke.hangoutcore.main;
+import org.bukkit.scheduler.BukkitTask;
 
 public class HungerDamageCheck implements Listener {
 
-    @EventHandler
-    //Cancel out vanilla hunger damage mechanic
-    public void playerDamageCheck(EntityDamageEvent e){
-        if(e.getCause() == EntityDamageEvent.DamageCause.STARVATION){
-            e.setCancelled(true);
 
-            // Set the player as the entity involved
-            Player player = (Player) e.getEntity();
-
-            /*
-             If food level is less than or at 0,
-             damage the player every 4 seconds by .5 hearts
-             until they eat or reach .5 total health
-             */
-            //Check player health is above 1 and empty food level
-            while(player.getHealth() > 1 && player.getFoodLevel() <= 0){
-                //try to sleep for 4 seconds followed by health deduction
-                try {
-                    TimeUnit.SECONDS.sleep(4);
-                    player.setHealth(player.getHealth() - 1);
-                    //If error occurred or interrupted go to catch
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-        }
-    }
 
 }
--- src/main/java/io/github/lyxke/hangoutcore/main.java
+++ src/main/java/io/github/lyxke/hangoutcore/main.java
@@ -6,11 +6,12 @@ import io.github.lyxke.hangoutcore.events.HungerDamageCheck;
 import io.github.lyxke.hangoutcore.events.PlayerHungerDamage;
 import io.github.lyxke.hangoutcore.events.PlayerWelcomeMessage;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.concurrent.TimeUnit;
-
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 public class main extends JavaPlugin implements Listener {
 
@@ -22,6 +23,7 @@ public class main extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(new PlayerHungerDamage(), this);
         getServer().getPluginManager().registerEvents(new SettingsCommand(), this);
         getServer().getPluginManager().registerEvents(new HungerDamageCheck(), this);
+        getServer().getPluginManager().registerEvents(this, this);
 
         getCommand("settings").setExecutor(new SettingsCommand());
         getCommand("hunger").setExecutor(new HungerCommand());
@@ -40,5 +42,32 @@ public class main extends JavaPlugin implements Listener {
         Player player = e.
     }
 */
+
+    @EventHandler
+    //Cancel out vanilla hunger damage mechanic
+    public void playerDamageCheck(EntityDamageEvent e){
+
+        if(e.getCause() == EntityDamageEvent.DamageCause.STARVATION){
+
+            e.setCancelled(true);
+
+            BukkitTask task = new BukkitRunnable() {
+                public void run() {
+
+                    // Set the player as the entity involved
+                    Player player = (Player) e.getEntity();
+                    /*
+                    If food level is less than or at 0,
+                     damage the player every 4 seconds by .5 hearts
+                     until they eat or reach .5 total health
+                     */
+                    //Check player health is above 1 and empty food level
+                    while(player.getHealth() > 1 && player.getFoodLevel() <= 0){
+                        player.setHealth(player.getHealth() - 1);
+                    }
+                }
+            }.runTaskTimer(this,0,80L);
+        }
+    }
 
 }
